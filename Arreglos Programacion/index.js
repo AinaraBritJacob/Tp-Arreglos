59fcbe910af631da31825ade83061be1854205ac
AinaraBritJacob <166017112+AinaraBritJacob@users.noreply.github.com>
2024-08-11 20:06:59 -0300

    Add files via upload
    Tp arreglos

---
+++ Arreglos Programacion/index.js
@@ -0,0 +1,18 @@
+//1)
+let jugadores= [ "Messi","Ronaldo", "Maradona","Neymar","Julian"];
+console.log(jugadores);
+//2)
+let puntajes=[];
+for (let i = 0; i < jugadores.length; i++) {
+
+    let puntajes=Number(prompt("Ingresa un puntaje"))
+    console.log(puntajes  +   jugadores [i])
+    
+}
+
+
+
+
+
+
+
