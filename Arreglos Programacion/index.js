226ab9653e781ce2bec1d5a5f602e666cb91d8d7
AinaraBritJacob <166017112+AinaraBritJacob@users.noreply.github.com>
2024-08-12 08:10:48 -0300

    Update index.js

--- Arreglos Programacion/index.js
+++ Arreglos Programacion/index.js
@@ -12,6 +12,36 @@ for (let i = 0; i < jugadores.length; i++) {
 
 
 
+//1)
+let Jugadores = ["Lucia","Lopez","Caesar","Jose","Javier"]
+
+//2)
+for (let i = 1; i <= 5; i++) {
+    let nombre = prompt(`Introduce el nombre del jugador ${i}:`);
+    let puntaje = parseInt(prompt(`Introduce el puntaje de ${nombre}:`), 10);
+}
+
+let Puntajes = []
+jugadores.push({ nombre: nombre, puntaje: puntaje });
+
+//3)
+
+let PuntajeMayor = Puntajes[0]
+let jugadormayor = jugadores[0]
+for (let indice = 0; indice< Puntajes.length; indice = indice + 1) {
+    if(Puntajes[indice] > Cantidad){
+        Cantidad = Puntajes[indice]
+    }
+    
+}
+//4)
+console.log("El puntaje mas bajo fue" + Cantidad)
+//5)
+Jugadores.push(prompt("Ingrese nombre de jugador"))
+Puntajes.push(prompt("Ingrese puntaje de Jugador"))
+//6)
+console.log(`Cantidad de puntajes guardados: {jugadores.length}`);
+//7)
 
 
 
